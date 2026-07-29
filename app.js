2267e4ea539f0b1df1487f39409025d74da2814b
Preetam Barse <87484824+Preetam13@users.noreply.github.com>
2026-07-29 11:46:47 +0530

    Initialize basic HTTP server in app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Content-Type": "text/plain"
+    });
+
+    res.end("GitHub Actions Practice");
+}).listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
